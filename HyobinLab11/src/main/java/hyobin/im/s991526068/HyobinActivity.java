6d2395ad6033d2d32ea23e50681d2c53d7e13d95
ihb92 <jakeim9@gmail.com>
2021-01-31 18:31:27 -0500

    Added two buttons on the main, created toast and made button 1 to go to the website of choice (youtube) and show name and site link in toast

--- HyobinLab11/src/main/java/hyobin/im/s991526068/HyobinActivity.java
+++ HyobinLab11/src/main/java/hyobin/im/s991526068/HyobinActivity.java
@@ -6,7 +6,14 @@ package hyobin.im.s991526068;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class HyobinActivity extends AppCompatActivity {
 
@@ -14,5 +21,31 @@ public class HyobinActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void callIntent(View view) {
+        Intent intent = null;
+        switch (view.getId()) {
+            case R.id.buttonToast:
+                Context context = getApplicationContext();
+                CharSequence text = getString(R.string.toast_label);
+                int duration = Toast.LENGTH_LONG;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+
+                intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse(getString(R.string.site_label)));
+                startActivity(intent);
+                break;
+            case R.id.buttonSnackbar:
+                Snackbar.make(view, "The snackbar was clicked", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                intent = new Intent(this, ImActivity.class);
+                startActivity(intent);
+            default:
+                break;
+
+        }
     }
 }
