23b4a4a0f228035b41309ba173e71429e65d6e4c
ihb92 <jakeim9@gmail.com>
2021-01-31 16:05:54 -0500

    Initial commit

---
+++ HyobinLab11/src/main/java/hyobin/im/s991526068/ImActivity.java
@@ -0,0 +1,18 @@
+/*
+Hyobin Im s991526068
+This is assignment 1 to demonstrate the use of activities and how to invoke 3rd party app
+ */
+package hyobin.im.s991526068;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_im);
+    }
+}
